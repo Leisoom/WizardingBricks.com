af75cf405e7946bbc5d8c674a3ce9218700c7fb4
Leissom <leisoom.gg@gmail.com>
2025-10-25 09:16:02 -0700

    add initial model schema

---
+++ WizardingBricks/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WizardingBricks.Models;
+public class AppDbContext : DbContext
+{
+    public DbSet<Set> Sets { get; set; }
+    public DbSet<Series> Series { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
+
---
+++ WizardingBricks/Models/Set.cs
@@ -0,0 +1,15 @@
+﻿namespace WizardingBricks.Models;
+public class Set
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Type { get; set; } = null!;
+    public int Year { get; set; }
+    public int Number_Of_Parts { get; set; }
+
+    public int SeriesId { get; set; }
+    public Series Series { get; set; } = null!;
+
+}
+
