793c7b266b64b25a322b44b5050aac0c25d9bd11
Leissom <leisoom.gg@gmail.com>
2025-10-25 21:44:10 -0700

    add lego specific properties to set model

--- WizardingBricks/Migrations/20251026044248_lego_set_number_added.Designer.cs
+++ WizardingBricks/Migrations/20251026044248_lego_set_number_added.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WizardingBricks.Models;
 
@@ -10,9 +11,11 @@ using WizardingBricks.Models;
 namespace WizardingBricks.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251026044248_lego_set_number_added")]
+    partial class legoSetNumberAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WizardingBricks/Migrations/20251026044248_lego_set_number_added.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WizardingBricks.Migrations
+{
+    /// <inheritdoc />
+    public partial class lego_set_number_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Preview_URL",
+                table: "Sets",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Set_Number",
+                table: "Sets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "Sets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Preview_URL",
+                table: "Sets");
+
+            migrationBuilder.DropColumn(
+                name: "Set_Number",
+                table: "Sets");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Sets");
+        }
+    }
+}
