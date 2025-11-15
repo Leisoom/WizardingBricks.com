ba263b7b070f2678a1f83c789c89a0644136bfe7
Leissom <leisoom.gg@gmail.com>
2025-11-15 02:38:35 -0800

    Allow for minifigures not based on characters to be added to the db

---
+++ WizardingBricks/Migrations/20251115102639_MakeCharacterNullableInMinifigureModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WizardingBricks.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCharacterNullableInMinifigureModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Minifigures_Characters_CharacterId",
+                table: "Minifigures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CharacterId",
+                table: "Minifigures",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Minifigures_Characters_CharacterId",
+                table: "Minifigures",
+                column: "CharacterId",
+                principalTable: "Characters",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Minifigures_Characters_CharacterId",
+                table: "Minifigures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CharacterId",
+                table: "Minifigures",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Minifigures_Characters_CharacterId",
+                table: "Minifigures",
+                column: "CharacterId",
+                principalTable: "Characters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- WizardingBricks/Models/Minifigure.cs
+++ WizardingBricks/Models/Minifigure.cs
@@ -8,7 +8,7 @@ public class Minifigure
     public int Number_Of_Parts { get; set; }
     public string Preview_Image_URL { get; set; } = null!;
 
-    public Character Character { get; set; } = null!;
+    public Character? Character { get; set; }
     public ICollection<Set> Sets { get; set; } = [];
 }
 
